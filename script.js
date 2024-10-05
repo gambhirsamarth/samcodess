3ec82068d13f03945d34f6d7d7eddb317c2befa1
samarth <samarth.gambhir@kapturecrm.com>
2024-10-06 00:28:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        light: "#F5F5F5",
+        charcoal: "#1A1A1A",
+        orange: "#FF8C42",
+        gray: "#C4C4C4",
+      },
+    },
+  },
+};
